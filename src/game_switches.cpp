fd83e07bf1219826fdb55b4ace6be811ec7120bc
Tora Phan Nam <admin@s1432.org>
2025-07-11 15:15:58 +0700

    Update game_switches.cpp

--- src/game_switches.cpp
+++ src/game_switches.cpp
@@ -40,7 +40,13 @@ bool Game_Switches::Set(int switch_id, bool value) {
 		ss.resize(switch_id);
 	}
 	ss[switch_id - 1] = value;
-	if (switch_id == Speedrun::COMPLETED_SWITCH && value == true) Speedrun::Complete();
+	if (switch_id == Speedrun::COMPLETED_SWITCH && value == true) {
+		EM_ASM_({
+			Speedrun.stop();
+		});
+		Speedrun::StopPing();
+		Speedrun::Complete();
+	}
 	if (switch_id == Speedrun::REDIRECT_SWITCH && value == true) EM_ASM({
 		document.location.href = UTF8ToString($0);
 	}, Speedrun::REDIRECT_URL.c_str());
